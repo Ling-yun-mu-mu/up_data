a2ca2a3ec3f215cdb2f99bff5407f70d944cbc99
Ling-yun-mu-mu <101865790+Ling-yun-mu-mu@users.noreply.github.com>
2022-03-20 15:03:55 +0800

    Update up_data_group.py

--- up_data_group.py
+++ up_data_group.py
@@ -120,9 +120,9 @@ if __name__ == '__main__':
         print(login_data)
 
         req = requests.session()
-        req.post(login_url,headers=login_headers,data=login_data)
+        req.post(login_url,headers=login_headers,data=login_data,proxies=proxies)
 
-        res = req.get(imformation_url,headers=updata_headers)
+        res = req.get(imformation_url,headers=updata_headers,proxies=proxies)
         soup = BeautifulSoup(res.content.decode('utf-8'),'lxml')
 
         try:
@@ -130,7 +130,7 @@ if __name__ == '__main__':
             token_value = tokens.attrs['value']
             updata_data['ttoken'] = token_value
 
-            res = req.post(updata_url,headers=updata_headers,data=updata_data)
+            res = req.post(updata_url,headers=updata_headers,data=updata_data,proxies=proxies)
         except Exception as e:
             with open('log.txt','a') as f:
                 f.write(i['姓名']+'：失败 '+str(e))
