8efcbcb3af2690d467240d1d11595de3a1b5c504
Ling-yun-mu-mu <101865790+Ling-yun-mu-mu@users.noreply.github.com>
2022-03-20 16:36:52 +0800

    Update up_data_group.py

--- up_data_group.py
+++ up_data_group.py
@@ -8,7 +8,7 @@ import datetime
     #"http": 'http://118.190.244.234:3128'
     #"http": 'http://152.136.62.181:9999'
 proxies = {
-    "http": 'http://47.100.255.35:80'
+    "http": '124.70.94.247:3128'
 }
 
 login_url = "http://yiqing.ctgu.edu.cn/wx/index/loginSubmit.do"
